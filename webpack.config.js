b22da953adcfb6f1b73151db7450c83b0da4809d
Philip Parke <philip@silverorange.com>
2017-10-10 19:42:07 -0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader" // creates style nodes from JS strings
+				}, {
+					loader: "css-loader" // translates CSS into CommonJS
+				}, {
+					loader: "sass-loader" // compiles Sass to CSS
+				}]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	}
+};
